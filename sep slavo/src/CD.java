2f3c77594de7d1031c366e3e3aad2a8f931e2f3a
Slavomir Simko <slavomir.simko@outlook.com>
2014-11-26 19:26:54 +0100

    sep
    update 0.04

---
+++ sep slavo/src/CD.java
@@ -0,0 +1,82 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+
+public class CD extends Item
+{
+   private boolean availability;
+   private Date borrowedFrom;
+   private String title;
+   private ArrayList<CD> itemsOfTheSameTitle;
+   
+   public CD(String title)
+   {
+      this.title = title;
+      this.itemsOfTheSameTitle = new ArrayList<CD>();
+      this.availability = true;
+   }
+   
+   public void addTheItemOfTheSameType()
+   {
+      this.itemsOfTheSameTitle.add(new CD(this.title));
+   }
+   
+   public void deleteTheItem(int index)
+   {
+      this.itemsOfTheSameTitle.remove(index);
+   }
+   
+   public CD getTheItem(int index)
+   {
+      return this.itemsOfTheSameTitle.get(index);
+   }
+   
+   @Override
+   public void setAvailability(boolean availability,int index)
+   {
+      this.itemsOfTheSameTitle.get(index).availability=availability;
+   }
+   
+   @Override
+   public void setBorrowedFrom(int index)
+   {
+      this.itemsOfTheSameTitle.get(index).borrowedFrom = new Date();
+   }
+   
+   @Override
+   public boolean getAvailability(int index)
+   {
+      return this.itemsOfTheSameTitle.get(index).availability;
+   }
+   
+   @Override
+   public Date getBorrowedFrom(int index)
+   {
+      return this.itemsOfTheSameTitle.get(index).borrowedFrom;
+   }
+   
+   @Override
+   public String getTitle()
+   {
+      return this.title;
+   }
+   
+   @Override
+   public int getNumberOfCopies()
+   {
+      return this.itemsOfTheSameTitle.size();
+   }
+   
+   @Override
+   public int getIndexOfAvailableCopy()
+   {
+      int indexOfAvailableCopy = -1;   
+         for (int i = this.itemsOfTheSameTitle.size()-1; i>=0; i--)
+         {
+            if (this.itemsOfTheSameTitle.get(i).availability == true)
+               indexOfAvailableCopy = i;
+         }
+      
+      return indexOfAvailableCopy;
+   }
+}
---
+++ sep slavo/src/Customer.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+
+
+public class Customer
+{
+   private String name;
+   private String cpr;
+   private String type;
+   private ArrayList<Borrowing> borrowings;
+   private ArrayList<Reservation> reservations;
+   
+   public Customer(String name, String cpr, String type)
+   {
+      this.name = name;
+      this.cpr = cpr;
+      this.type = type;
+      this.borrowings = new ArrayList<Borrowing>();
+      this.reservations = new ArrayList<Reservation>();
+   }
+   
+   public String toString()
+   {
+      return this.name + " " + this.cpr + " " + this.type;
+   }
+   
+   public void newBorrowing(String detail, Item item)
+   {
+      int index = item.getIndexOfAvailableCopy();
+      if (index != -1)
+      this.borrowings.add(new Borrowing(detail,item,index));
+   }
+   
+   public void newReservation(String detail, Item item)
+   {
+      int index = item.getIndexOfAvailableCopy();
+      if (index != -1)
+      this.reservations.add(new Reservation(detail,item,index));
+   }
+   
+   public String reservationsToString()
+   {
+      String message = "";
+      for (int i=0; i<this.reservations.size(); i++)
+      {
+         message += this.reservations.get(i).toString();
+         message += "\n";
+      }
+      return message;
+   }
+   
+   public String borrowingsToString()
+   {
+      String message = "";
+      for (int i=0; i<this.borrowings.size(); i++)
+      {
+         message += "res num " + (i+1) + " ";
+         message += this.borrowings.get(i).toString();
+         message += "\n";
+      }
+      return message;
+   }
+   
+   public void deleteReservation(int numberOfCopy)
+   {
+      this.reservations.remove(numberOfCopy-1);
+   }
+   
+   public void deleteBorrowing(int numberOfCopy)
+   {
+      this.borrowings.remove(numberOfCopy-1);
+   }
+   
+   public String getName()
+   {
+      return name;
+   }
+}
